d8555fc241f9958785b40e13010d70926de6c3b5
Damien Szczyt <dszczyt@work4labs.com>
2022-12-04 22:08:29 +0100

    day3 part 2

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -1,5 +1,5 @@
 #[derive(Clone, Debug)]
-struct Rucksack {
+pub struct Rucksack {
     pub items: String,
 }
 
@@ -13,17 +13,34 @@ impl Rucksack {
     }
 
     pub fn priority(self) -> usize {
-        match self
-            .common_in_compartments()
-            .chars()
-            .nth(0)
-            .unwrap_or_default()
-        {
-            c @ ('a'..='z') => c as usize - ('a' as usize) + 1,
-            c @ ('A'..='Z') => c as usize - ('A' as usize) + 27,
-            _ => 0,
-        }
+        priority(
+            self.common_in_compartments()
+                .chars()
+                .nth(0)
+                .unwrap_or_default(),
+        )
     }
+}
+
+pub fn priority(c: char) -> usize {
+    match c {
+        c @ ('a'..='z') => c as usize - ('a' as usize) + 1,
+        c @ ('A'..='Z') => c as usize - ('A' as usize) + 27,
+        _ => 0,
+    }
+}
+
+pub fn rucksacks_priority(rucksacks: Vec<Rucksack>) -> usize {
+    priority(
+        common_chars_3(
+            rucksacks.get(0).unwrap().items.clone(),
+            rucksacks.get(1).unwrap().items.clone(),
+            rucksacks.get(2).unwrap().items.clone(),
+        )
+        .chars()
+        .nth(0)
+        .unwrap_or_default(),
+    )
 }
 
 #[derive(Clone, Debug)]
@@ -58,7 +75,18 @@ pub fn common_chars(s1: String, s2: String) -> String {
     String::from_iter(chars)
 }
 
-struct Rucksacks {
+pub fn common_chars_3(s1: String, s2: String, s3: String) -> String {
+    let mut chars: Vec<char> = s1
+        .chars()
+        .filter(|c1| s2.chars().any(|c2| c2.eq(&c1)))
+        .filter(|c1| s3.chars().any(|c3| c3.eq(&c1)))
+        .collect();
+    chars.dedup();
+    String::from_iter(chars)
+}
+
+#[derive(Clone, Debug)]
+pub struct Rucksacks {
     pub rucksacks: Vec<Rucksack>,
 }
 
@@ -84,9 +112,66 @@ impl Rucksacks {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct Group {
+    pub rucksacks: Rucksacks,
+}
+
+impl Group {
+    pub fn sum_priorities(self) -> usize {
+        rucksacks_priority(self.rucksacks.rucksacks)
+    }
+}
+
+pub struct Groups {
+    pub groups: Vec<Group>,
+}
+
+impl Groups {
+    pub fn sum_priorities(self) -> usize {
+        self.groups
+            .iter()
+            .map(|group| group.clone().sum_priorities())
+            .sum()
+    }
+}
+
+impl From<&str> for Groups {
+    fn from(input: &str) -> Self {
+        let groups: Vec<Group> = input
+            .lines()
+            .enumerate()
+            .fold(Vec::new(), |mut acc, (i, line)| {
+                if i % 3 == 0 {
+                    acc.push(vec![line]);
+                } else {
+                    let last = acc.last_mut().unwrap();
+                    last.push(line);
+                }
+                acc
+            })
+            .iter()
+            .map(|elems| Group {
+                rucksacks: Rucksacks {
+                    rucksacks: elems
+                        .iter()
+                        .map(|elem| Rucksack {
+                            items: elem.to_string(),
+                        })
+                        .collect(),
+                },
+            })
+            .collect();
+        groups.iter().for_each(|e| {
+            dbg!(&e);
+        });
+        Self { groups }
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{common_chars, Compartment, Rucksack, Rucksacks};
+    use crate::{common_chars, common_chars_3, Compartment, Groups, Rucksack, Rucksacks};
 
     #[test]
     fn test_rucksack_to_vec_of_compartments_1() {
@@ -187,10 +272,31 @@ mod test {
         let rucksacks: Rucksacks = include_str!("example").into();
         assert_eq!(rucksacks.sum_priorities(), 157);
     }
+
+    macro_rules! test_common_chars_3 {
+      ($($name:ident: ($a:expr, $b:expr, $c:expr, $expected:expr))*) => {$(
+        #[test]
+        fn $name() {
+          assert_eq!(common_chars_3($a.to_string(), $b.to_string(), $c.to_string()), $expected.to_string())
+        }
+      )*};
+  }
+
+    test_common_chars_3! {
+      test_common_chars_3_1: ("vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg", "r")
+      test_common_chars_3_2: ("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw", "Z")
+    }
+
+    #[test]
+    fn test_groups_sum_priotities() {
+        let input = include_str!("example");
+        let rucksacks: Groups = input.into();
+        assert_eq!(rucksacks.sum_priorities(), 70);
+    }
 }
 
 fn main() {
     let input = include_str!("input");
-    let rucksacks: Rucksacks = input.into();
+    let rucksacks: Groups = input.into();
     dbg!(rucksacks.sum_priorities());
 }
