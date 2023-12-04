3cf35f5632c0ceb0fca4170d17e2689028db8a61
Damien Szczyt <dszczyt@work4labs.com>
2023-12-04 08:06:53 +0100

    day3 part2

--- 2023/src/bin/day3/main.rs
+++ 2023/src/bin/day3/main.rs
@@ -67,6 +67,65 @@ pub fn numbers(input: &str) -> Vec<u64> {
     result
 }
 
+fn find_stars(input: &str) -> Vec<(usize, usize)> {
+    let mut result = vec![];
+
+    input.lines().enumerate().for_each(|(j, line)| {
+        line.chars().enumerate().for_each(|(i, ch)| {
+            if ch == '*' {
+                result.push((i, j));
+            }
+        })
+    });
+
+    result
+}
+
+fn numbers_next_to_star(
+    star: (usize, usize),
+    row: usize,
+    numbers: Vec<(u64, Vec<usize>)>,
+) -> Vec<u64> {
+    let mut result = vec![];
+
+    let min_i = if star.0 == 0 { 0 } else { star.0 - 1 };
+    let min_j = if star.1 == 0 { 0 } else { star.1 - 1 };
+
+    numbers.iter().for_each(|(number, positions)| {
+        for y in min_j..=star.1 + 1 {
+            for x in min_i..=star.0 + 1 {
+                for position in positions {
+                    if x == *position && y == row {
+                        result.push(*number);
+                        return;
+                    }
+                }
+            }
+        }
+    });
+
+    result
+}
+
+pub fn gear_ratios(input: &str) -> Vec<u64> {
+    let mut result = vec![];
+
+    let stars = find_stars(input);
+
+    stars.iter().for_each(|star| {
+        let mut numbers = vec![];
+        for (row, line) in input.lines().enumerate() {
+            let numbers_and_positions = extract_number_and_positions(line);
+            numbers.extend(numbers_next_to_star(*star, row, numbers_and_positions));
+        }
+        if numbers.len() == 2 {
+            result.push(numbers[0] * numbers[1]);
+        }
+    });
+
+    result
+}
+
 fn main() {
     let input = include_str!("inputs/part1");
 
@@ -74,6 +133,11 @@ fn main() {
 
     let total: u64 = numbers.iter().sum();
     println!("part1: {}", total);
+
+    let gear_ratios = gear_ratios(input);
+    let total: u64 = gear_ratios.iter().sum();
+
+    println!("part2: {}", total);
 }
 
 #[cfg(test)]
@@ -90,5 +154,14 @@ mod tests {
 
         let total: u64 = numbers.iter().sum();
         assert_eq!(total, 4361);
+    }
+
+    #[test]
+    fn test_gear_ratios() {
+        let input = include_str!("samples/part1");
+
+        let gear_ratios = super::gear_ratios(input);
+
+        assert_eq!(gear_ratios, vec![16345, 451490]);
     }
 }
